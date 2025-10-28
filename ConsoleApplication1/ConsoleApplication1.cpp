9c2616d683c976c549cc94cea06099eb3c1ffcc7
Yakov_eq20 <denisyakovlev133@gmail.com>
2025-10-28 16:31:33 +0200

    створили змінні, створили клас

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,30 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+// «м≥на - це ≥менована область пам'€т≥, €ка збер≥гаЇ ≥нформац≥ю певного типу
+// ≥ може зм≥нювати це значенн€ прот€гом виконанн€ програми
+// тип даних - характеристика даних, що визначаЇ:
+// 1) д≥апозон значень 
+// 2) наб≥р доступних операц≥й
+
+class Account {
+	string username; // зм≥нн≥-члени, характеристики, атрибути
+	string password;
+	string desc;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Account myAccount;
+
+	// int - тип даних €кий дозвол€Ї зберегати ц≥л≥ числа
+	int num = 120;
+	cout << num << endl;
+	num = 20;
+	cout << num << endl;
+
+
+	cout << 10 + 12 << endl;
+
+	
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
